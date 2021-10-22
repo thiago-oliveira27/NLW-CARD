37b814c2b48e03442fffedf4feb526371536529b
Thiago Oliveira Carvalho <78443004+thiago-oliveira27@users.noreply.github.com>
2021-10-22 20:12:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'thiago-oliveira27',
+  youtube: 'RocketSeat',
+  facebook: 'profile.php?id=100006556831788',
+  instagram: 'thiago_oliveira27',
+  twitter: 'thi_027'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
